f9aaac8760ffc400aa22e0c93f16ae2ba51d1f1b
Aqsa <aqsa65020@gmail>
2017-12-29 17:00:26 +0500

    commit by aqsa

--- app/src/main/java/uberapp/itpvt/com/alquranmyapp/Adapters/SurahNameAdapter.java
+++ app/src/main/java/uberapp/itpvt/com/alquranmyapp/Adapters/SurahNameAdapter.java
@@ -2,17 +2,31 @@ package uberapp.itpvt.com.alquranmyapp.Adapters;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Environment;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.HashMap;
 import java.util.List;
 
+import uberapp.itpvt.com.alquranmyapp.Activities.MainActivity;
 import uberapp.itpvt.com.alquranmyapp.Activities.SurahTranslation;
 import uberapp.itpvt.com.alquranmyapp.R;
+import uberapp.itpvt.com.alquranmyapp.SoundActivity;
 
 /**
  * Created by Mahek on 12/27/2017.
@@ -23,6 +37,8 @@ public class SurahNameAdapter extends RecyclerView.Adapter<SurahNameAdapter.Sura
     Context context;
     List<HashMap<String,String>> mapList;
     HashMap<String,String> mapItem;
+    String urlDownloadLink;
+    String downloadAudioPath;
 
 
     public SurahNameAdapter(Context context, List<HashMap<String,String>> mapList){
@@ -61,7 +77,41 @@ public class SurahNameAdapter extends RecyclerView.Adapter<SurahNameAdapter.Sura
                 context.getApplicationContext().startActivity(intent);
             }
         });
+        holder.tvSurahNameEnglish.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mapItem = mapList.get(position);
+                Intent intent = new Intent(context.getApplicationContext(), SurahTranslation.class);
+                intent.putExtra("ayahNumber", mapItem.get("number") );
+                //  context.startActivity(intent);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                context.getApplicationContext().startActivity(intent);
+            }
+        });
+        holder.ivDownload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                urlDownloadLink = "http://192.168.10.4/alquran/114.mp3";
 
+                downloadAudioPath = Environment.getExternalStorageDirectory().getAbsolutePath();
+                File audioVoice = new File(downloadAudioPath + File.separator + "Alquran");
+                if(!audioVoice.exists()){
+                    audioVoice.mkdir();
+                }
+
+
+                if(urlDownloadLink.equals("")){
+                    Toast.makeText(context.getApplicationContext(), "Please add audio download link", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                String filename = extractFilename();
+                downloadAudioPath = downloadAudioPath + File.separator + "Alquran" + File.separator + filename;
+                SurahNameAdapter.DownloadFile downloadAudioFile = new SurahNameAdapter.DownloadFile();
+                downloadAudioFile.execute(urlDownloadLink, downloadAudioPath);
+
+
+            }
+        });
 
 
     }
@@ -74,14 +124,98 @@ public class SurahNameAdapter extends RecyclerView.Adapter<SurahNameAdapter.Sura
     public class SurahNameHolder extends RecyclerView.ViewHolder{
 
 
-    TextView tvSurahNameArabic, tvSurahNameEnglish;
+    TextView tvSurahNameArabic, tvSurahNameEnglish,tvMessage;
+    ImageView ivDownload;
 
     public SurahNameHolder(View itemView) {
         super(itemView);
 
+     //   tvMessage = itemView.findViewById(R.id.tvMessage);
         tvSurahNameArabic = itemView.findViewById(R.id.tvSurahNameArabic);
         tvSurahNameEnglish = itemView.findViewById(R.id.tvSurahNameEnglish);
-
+        ivDownload = itemView.findViewById(R.id.ivDownload);
     }
 }
+
+
+
+
+
+
+//////////////////////////////////DOWNLOAD//////////////////////////////////////////////////
+private class DownloadFile extends AsyncTask<String, Integer, String> {
+
+    @Override
+    protected String doInBackground(String... url) {
+        int count;
+        try {
+            URL urls = new URL(url[0]);
+            URLConnection connection = urls.openConnection();
+            connection.connect();
+            // this will be useful so that you can show a tipical 0-100% progress bar
+            int lenghtOfFile = connection.getContentLength();
+
+            InputStream input = new BufferedInputStream(urls.openStream());
+            OutputStream output = new FileOutputStream(url[1]);
+
+            byte data[] = new byte[1024];
+
+            long total = 0;
+
+            while ((count = input.read(data)) != -1) {
+                total += count;
+                // publishing the progress....
+                publishProgress((int) (total * 100 / lenghtOfFile));
+                output.write(data, 0, count);
+            }
+
+            output.flush();
+            output.close();
+            input.close();
+        } catch (Exception e) {
+        }
+        return null;
+    }
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+        MainActivity.progressbar.setVisibility(ProgressBar.VISIBLE);
+        MainActivity.tvMessage.setVisibility(TextView.VISIBLE);
+
+    }
+
+    @Override
+    protected void onPostExecute(String s) {
+        super.onPostExecute(s);
+        MainActivity.progressbar.setVisibility(ProgressBar.GONE);
+        MainActivity.tvMessage.setVisibility(TextView.GONE);
+    }
+}
+
+    private String extractFilename(){
+        if(urlDownloadLink.equals("")){
+            return "";
+        }
+        String newFilename = "";
+        if(urlDownloadLink.contains("/")){
+            int dotPosition = urlDownloadLink.lastIndexOf("/");
+            newFilename = urlDownloadLink.substring(dotPosition + 1, urlDownloadLink.length());
+        }
+        else{
+            newFilename = urlDownloadLink;
+        }
+        return newFilename;
+    }
+
+
+
+
+
+
+
+
+
+
+
 }
--- app/src/main/java/uberapp/itpvt/com/alquranmyapp/SoundActivity.java
+++ app/src/main/java/uberapp/itpvt/com/alquranmyapp/SoundActivity.java
@@ -1,42 +1,91 @@
 package uberapp.itpvt.com.alquranmyapp;
 
+import android.content.Intent;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
+import android.os.AsyncTask;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLConnection;
 
 public class SoundActivity extends AppCompatActivity {
 
     Button btnPlay;
     String url ;
+    private String downloadAudioPath;
+    private String urlDownloadLink = "";
+    private ProgressBar progressbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sound);
 
         btnPlay = (Button) findViewById(R.id.btnPlay);
+        downloadAudioPath = Environment.getExternalStorageDirectory().getAbsolutePath();
+        File audioVoice = new File(downloadAudioPath + File.separator + "Alquran");
+        if(!audioVoice.exists()){
+            audioVoice.mkdir();
+        }
+
+
+        urlDownloadLink = "http://192.168.10.4/alquran/114.mp3";
+
+
+        progressbar = (ProgressBar)findViewById(R.id.progress_view);
+
+        MediaPlayer mediaPlayer = new MediaPlayer();
+        mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        try {
+            mediaPlayer.setDataSource(urlDownloadLink);
+            mediaPlayer.prepare();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+//                mediaPlayer.prepare(); // might take long! (for buffering, etc)
+        mediaPlayer.start();
 
         btnPlay.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
 
-                File file = new File("114");
-                if(file.exists()){
-                    Toast.makeText(getApplicationContext(),"File Exists",Toast.LENGTH_SHORT).show();
-                }else{
+
+
+                if(urlDownloadLink.equals("")){
+                    Toast.makeText(SoundActivity.this, "Please add audio download link", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                String filename = extractFilename();
+                downloadAudioPath = downloadAudioPath + File.separator + "voices" + File.separator + filename;
+                DownloadFile downloadAudioFile = new DownloadFile();
+                downloadAudioFile.execute(urlDownloadLink, downloadAudioPath);
+               // audioText.setText("");
+            }
+
+
+//                File file = new File("114");
+//                if(file.exists()){
+//                    Toast.makeText(getApplicationContext(),"File Exists",Toast.LENGTH_SHORT).show();
+//                }else{
+
+
 //                    url = ;
 //                    URL url = "http://localhost/alquran/114.mp3";
 //                    url = new URL(url);
@@ -66,30 +115,83 @@ public class SoundActivity extends AppCompatActivity {
 
 
 
-                }
-
-
-
-
-                MediaPlayer mediaPlayer = new MediaPlayer();
-                mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
-                try {
-                    mediaPlayer.setDataSource(url);
-                    mediaPlayer.prepare();
-
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-//                mediaPlayer.prepare(); // might take long! (for buffering, etc)
-                mediaPlayer.start();
+               // }
 
 
 
 
 
-            }
+
+
+
+//            }
         });
 
 
     }
+
+
+    private class DownloadFile extends AsyncTask<String, Integer, String> {
+
+        @Override
+        protected String doInBackground(String... url) {
+            int count;
+            try {
+                URL urls = new URL(url[0]);
+                URLConnection connection = urls.openConnection();
+                connection.connect();
+                // this will be useful so that you can show a tipical 0-100% progress bar
+                int lenghtOfFile = connection.getContentLength();
+
+                InputStream input = new BufferedInputStream(urls.openStream());
+                OutputStream output = new FileOutputStream(url[1]);
+
+                byte data[] = new byte[1024];
+
+                long total = 0;
+
+                while ((count = input.read(data)) != -1) {
+                    total += count;
+                    // publishing the progress....
+                    publishProgress((int) (total * 100 / lenghtOfFile));
+                    output.write(data, 0, count);
+                }
+
+                output.flush();
+                output.close();
+                input.close();
+            } catch (Exception e) {
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            progressbar.setVisibility(ProgressBar.VISIBLE);
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            progressbar.setVisibility(ProgressBar.GONE);
+        }
+    }
+
+    private String extractFilename(){
+        if(urlDownloadLink.equals("")){
+            return "";
+        }
+        String newFilename = "";
+        if(urlDownloadLink.contains("/")){
+            int dotPosition = urlDownloadLink.lastIndexOf("/");
+            newFilename = urlDownloadLink.substring(dotPosition + 1, urlDownloadLink.length());
+        }
+        else{
+            newFilename = urlDownloadLink;
+        }
+        return newFilename;
+    }
+
+
 }
