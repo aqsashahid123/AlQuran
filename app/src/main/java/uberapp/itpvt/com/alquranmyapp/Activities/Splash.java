98ce47277e0020f776434c82be01876ccfef7530
zahidalichaudhry <zahidalichaudhry803@gmail.com>
2018-01-01 15:22:24 +0500

    Zahid

---
+++ app/src/main/java/uberapp/itpvt/com/alquranmyapp/Activities/Splash.java
@@ -0,0 +1,30 @@
+package uberapp.itpvt.com.alquranmyapp.Activities;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import uberapp.itpvt.com.alquranmyapp.R;
+
+public class Splash extends Activity {
+
+
+    // Splash screen apperance time (3.5 seconds)
+    private static int SPLASH_TIME_OUT = 3500;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(Splash.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
