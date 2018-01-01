9499eeac7a61dfc6075148873907b85065a1cc4b
Aqsa <aqsa65020@gmail>
2018-01-01 17:05:05 +0500

    commit by aqsa

--- app/src/main/java/uberapp/itpvt/com/alquranmyapp/Activities/MenuActivity.java
+++ app/src/main/java/uberapp/itpvt/com/alquranmyapp/Activities/MenuActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.LinearLayout;
@@ -25,6 +27,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
+import uberapp.itpvt.com.alquranmyapp.Adapters.SearchAdapter;
 import uberapp.itpvt.com.alquranmyapp.Adapters.SurahNameAdapter;
 import uberapp.itpvt.com.alquranmyapp.R;
 
@@ -35,8 +38,9 @@ public class MenuActivity extends AppCompatActivity {
     EditText etSearch;
     RecyclerView rv;
     List<HashMap<String,String>> mapList;
+    ArrayList<String> searchData;
     HashMap<String,String> listItem;
-    SurahNameAdapter adapter;
+    SearchAdapter adapter;
     private String urlJsonObj = "http://api.alquran.cloud/surah";
 
     @Override
@@ -46,14 +50,18 @@ public class MenuActivity extends AppCompatActivity {
         mapList = new ArrayList<>();
         listItem = new HashMap<>();
         etSearch = (EditText) findViewById(R.id.tvSearch);
-
-
+        lvSearch = (LinearLayout) findViewById(R.id.lvSearch);
+        lvRead = (LinearLayout) findViewById(R.id.lvRead);
+        lvListen = (LinearLayout) findViewById(R.id.lvListen);
+        searchData = new ArrayList<>();
         lvSearch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
 
-
+                lvListen.setVisibility(View.GONE);
+                lvRead.setVisibility(View.GONE);
+                rv.setVisibility(View.VISIBLE);
 
 
             }
@@ -68,7 +76,7 @@ public class MenuActivity extends AppCompatActivity {
                     public void onResponse(String response) {
                         // Display the first 500 characters of the response string.
 
-                        Toast.makeText(MenuActivity.this, response, Toast.LENGTH_SHORT).show();
+                   //     Toast.makeText(MenuActivity.this, response, Toast.LENGTH_SHORT).show();
                         try {
                             JSONObject obj = new JSONObject(response);
                             JSONArray data = obj.getJSONArray("data");
@@ -81,16 +89,17 @@ public class MenuActivity extends AppCompatActivity {
                                 listItem.put("englishName",surahNameData.getString("englishName") );
                                 listItem.put("name",surahNameData.getString("name"));
                                 listItem.put("number", surahNameData.getString("number"));
+                                searchData.add(surahNameData.getString("name"));
                                 //listItem.put("surahNum")
                                 mapList.add(listItem);
                                 // listItem.clear();
                             }
                             //layoutManager = new LinearLayoutManager(getApplicationContext());
-                            LinearLayoutManager llm = new LinearLayoutManager(MenuActivity.this);
-                            adapter = new SurahNameAdapter(getApplicationContext(),mapList);
-
-                            rv.setAdapter(adapter);
-                            rv.setLayoutManager(llm);
+//                            LinearLayoutManager llm = new LinearLayoutManager(MenuActivity.this);
+//                            adapter = new SearchAdapter(getApplicationContext(),mapList,searchData);
+//
+//                            rv.setAdapter(adapter);
+//                            rv.setLayoutManager(llm);
 
                         }
 
@@ -113,7 +122,64 @@ public class MenuActivity extends AppCompatActivity {
         RequestQueue queue = Volley.newRequestQueue(this);
         queue.add(stringRequest);
 
+        etSearch.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean b) {
+                if (b){
 
+//                    lvListen.setVisibility(View.GONE);
+//                    lvRead.setVisibility(View.GONE);
+//                    rv.setVisibility(View.VISIBLE);
+
+
+                }
+                if (!b){
+                    lvListen.setVisibility(View.VISIBLE);
+                    lvRead.setVisibility(View.VISIBLE);
+                    rv.setVisibility(View.GONE);
+
+                }
+            }
+        });
+        etSearch.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+
+                String nameToSearch = etSearch.getText().toString();
+                ArrayList<String> filteredLeaves = new ArrayList<String>();
+
+                for (String data : searchData) {
+                    if (data.toLowerCase().contains(nameToSearch.toLowerCase()) ) {
+                        filteredLeaves.add(data);
+                    }
+
+
+                }
+                /*leaveDatas.clear();
+                leaveDatas.addAll(filteredLeaves);
+                leaves_adapter.notifyDataSetChanged();*/
+//                adapter = new SearchAdapter(MenuActivity.this,mapList, filteredLeaves);
+//                rv.setAdapter(adapter);
+                LinearLayoutManager llm = new LinearLayoutManager(MenuActivity.this);
+                adapter = new SearchAdapter(getApplicationContext(),mapList,filteredLeaves);
+
+                rv.setAdapter(adapter);
+                rv.setLayoutManager(llm);
+                rv.setVisibility(View.VISIBLE);
+
+            }
+            @Override
+            public void afterTextChanged(Editable s) {
+
+
+            }
+        });      //
 
 
 
---
+++ app/src/main/java/uberapp/itpvt/com/alquranmyapp/Adapters/SearchAdapter.java
@@ -0,0 +1,270 @@
+package uberapp.itpvt.com.alquranmyapp.Adapters;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Environment;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import uberapp.itpvt.com.alquranmyapp.Activities.MainActivity;
+import uberapp.itpvt.com.alquranmyapp.Activities.SurahTranslation;
+import uberapp.itpvt.com.alquranmyapp.R;
+
+/**
+ * Created by Mahek on 1/1/2018.
+ */
+
+public class SearchAdapter extends RecyclerView.Adapter<SearchAdapter.SearchHolder>{
+
+        Context context;
+        List<HashMap<String,String>> mapList;
+        ArrayList<String> searchData;
+        HashMap<String,String> mapItem;
+        String urlDownloadLink;
+        String downloadAudioPath;
+
+
+        public SearchAdapter(Context context, List<HashMap<String,String>> mapList,ArrayList<String> searchData){
+
+
+            this.context = context;
+            this.mapList = mapList;
+            this.searchData = searchData;
+        }
+
+
+        @Override
+        public SearchHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View itemView = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.row_surah_names, parent, false);
+
+            return new SearchHolder(itemView);
+
+            //return null;
+        }
+
+        @Override
+        public void onBindViewHolder(SearchHolder holder, final int position) {
+            mapItem = new HashMap<>();
+            mapItem = mapList.get(position);
+       //     holder.tvSurahNameEnglish.setText(mapItem.get("englishName"));
+            holder.tvSurahNameArabic.setText(searchData.get(position));
+
+            holder.tvSurahNameArabic.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    mapItem = mapList.get(position);
+                    Intent intent = new Intent(context.getApplicationContext(), SurahTranslation.class);
+                    intent.putExtra("ayahNumber", mapItem.get("number") );
+                    //  context.startActivity(intent);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    context.getApplicationContext().startActivity(intent);
+                }
+            });
+            holder.tvSurahNameEnglish.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    mapItem = mapList.get(position);
+                    Intent intent = new Intent(context.getApplicationContext(), SurahTranslation.class);
+                    intent.putExtra("ayahNumber", mapItem.get("number") );
+
+                    //  context.startActivity(intent);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    context.getApplicationContext().startActivity(intent);
+                }
+            });
+            holder.ivDownload.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+//                urlDownloadLink = "http://192.168.10.4/alquran/114.mp3";
+                    urlDownloadLink = "http://192.168.10.4/alquran/" + mapItem.get("number") + ".mp3";
+
+
+                    downloadAudioPath = Environment.getExternalStorageDirectory().getAbsolutePath();
+                    File audioVoice = new File(downloadAudioPath + File.separator + "Alquran");
+                    if(!audioVoice.exists()){
+                        audioVoice.mkdir();
+                    }
+
+
+                    if(urlDownloadLink.equals("")) {
+                        Toast.makeText(context.getApplicationContext(), "Please add audio download link", Toast.LENGTH_LONG).show();
+                        return;
+                    }
+//                                      }
+//                    String filename = extractFilename();
+//                    downloadAudioPath = downloadAudioPath + File.separator + "Alquran" + File.separator + filename;
+//                    uberapp.itpvt.com.alquranmyapp.Adapters.SurahNameAdapter.DownloadFile downloadAudioFile = new uberapp.itpvt.com.alquranmyapp.Adapters.SurahNameAdapter.DownloadFile();
+//                    downloadAudioFile.execute(urlDownloadLink, downloadAudioPath);
+
+
+                }
+            });
+//
+//        holder.play.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//            }
+//        });
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return searchData.size();
+        }
+
+        public class SearchHolder extends RecyclerView.ViewHolder{
+
+
+            TextView tvSurahNameArabic, tvSurahNameEnglish,tvMessage;
+            ImageView ivDownload;
+//    SeekBar seekBar;
+//    Button play,pause;
+
+            public SearchHolder(View itemView) {
+                super(itemView);
+
+                //   tvMessage = itemView.findViewById(R.id.tvMessage);
+                tvSurahNameArabic = itemView.findViewById(R.id.tvSurahNameArabic);
+                tvSurahNameEnglish = itemView.findViewById(R.id.tvSurahNameEnglish);
+                ivDownload = itemView.findViewById(R.id.ivDownload);
+//        seekBar=itemView.findViewById(R.id.seekBar);
+//        play=itemView.findViewById(R.id.start);
+//        pause=itemView.findViewById(R.id.stop);
+//        seekBar.setVisibility(View.GONE);
+
+            }
+        }
+
+
+
+
+
+
+        //////////////////////////////////DOWNLOAD//////////////////////////////////////////////////
+        private class DownloadFile extends AsyncTask<String, Integer, String> {
+
+            @Override
+            protected String doInBackground(String... url) {
+                int count;
+                try {
+                    URL urls = new URL(url[0]);
+                    URLConnection connection = urls.openConnection();
+                    connection.connect();
+                    // this will be useful so that you can show a tipical 0-100% progress bar
+                    int lenghtOfFile = connection.getContentLength();
+
+                    InputStream input = new BufferedInputStream(urls.openStream());
+                    OutputStream output = new FileOutputStream(url[1]);
+
+                    byte data[] = new byte[1024];
+
+                    long total = 0;
+
+                    while ((count = input.read(data)) != -1) {
+                        total += count;
+                        // publishing the progress....
+                        publishProgress((int) (total * 100 / lenghtOfFile));
+                        output.write(data, 0, count);
+                    }
+
+                    output.flush();
+                    output.close();
+                    input.close();
+                } catch (Exception e) {
+                }
+                return null;
+            }
+
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+                MainActivity.progressbar.setVisibility(ProgressBar.VISIBLE);
+                MainActivity.tvMessage.setVisibility(TextView.VISIBLE);
+
+            }
+
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+                MainActivity.progressbar.setVisibility(ProgressBar.GONE);
+                MainActivity.tvMessage.setVisibility(TextView.GONE);
+            }
+        }
+
+        private String extractFilename(){
+            if(urlDownloadLink.equals("")){
+                return "";
+            }
+            String newFilename = "";
+            if(urlDownloadLink.contains("/")){
+                int dotPosition = urlDownloadLink.lastIndexOf("/");
+                newFilename = urlDownloadLink.substring(dotPosition + 1, urlDownloadLink.length());
+            }
+            else{
+                newFilename = urlDownloadLink;
+            }
+            return newFilename;
+        }
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
